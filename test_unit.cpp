aad502edff97ac3e7960a34228d027dbb4c33a1b
DevonC <devonc@DevonCs-MacBook-Pro.local>
2021-04-29 21:59:54 -0700

    Added two tests

--- test_unit.cpp
+++ test_unit.cpp
@@ -15,8 +15,31 @@ void test_Vector_parameterized_constructor(void)
     TEST_CHECK_(v.len() == 10, "v.len(): Expected=%d, Actual=%d", vector_len, v.len());
 }
 
+void test_Vector_parameterized_constructor_invalid_arg(void)
+{
+    unsigned int vector_len = 0;
+    TEST_EXCEPTION(Vector{vector_len}, std::invalid_argument);
+}
 
+void test_Vector_stdvector_constructor(void)
+{
+    int num_elements = 10;
+    std::vector<double> vec;
+    for (int i = 0; i < num_elements; i++)
+    {
+        vec.push_back(i);
+    }
 
+    Vector v(vec);
+
+    TEST_CHECK_(v.len() == num_elements, "v.len(): Expected=%d, Actual=%d", num_elements, v.len());
+    TEST_CHECK_(vec.size() == 0, "vec.size(): Expected=%d, Actual=%lu", 0, vec.size());
+
+    for (int i = 0; i < num_elements; i++)
+    {
+        TEST_CHECK(v[i] == i);
+    }
+}
 
 
 
@@ -26,5 +49,7 @@ TEST_LIST =
 {
     { "void test_Vector_default_constructor(void);", test_Vector_default_constructor },
     { "void test_Vector_parameterized_constructor(void);", test_Vector_parameterized_constructor },
+    { "void test_Vector_parameterized_constructor_invalid_arg(void);", test_Vector_parameterized_constructor_invalid_arg },
+    { "void test_Vector_stdvector_constructor(void);", test_Vector_stdvector_constructor },
     { NULL }
 };
